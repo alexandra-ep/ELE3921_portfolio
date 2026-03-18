1f3698c0f4118007b2ad03653f0de590f55bc3d6
Alexandra Eline Pettersen <57686601+alexandra-ep@users.noreply.github.com>
2026-03-18 23:04:38 +0100

    Create registration form and authentication

--- elevate/urls.py
+++ elevate/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("classes/", views.classes_list, name="classes_list"),
     path("classes/<int:class_id>/", views.class_detail, name="class_detail"),
     path("instructors/", views.instructors_list, name="instructors_list"),
+    path("accounts/register/", views.register, name="register"),
 ]
--- elevate/views.py
+++ elevate/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import FitnessClass, Instructor
 
 
@@ -19,3 +21,24 @@ def instructors_list(request):
 def class_detail(request, class_id):
     fitness_class = get_object_or_404(FitnessClass, id=class_id)
     return render(request, "class_detail.html", {"fitness_class": fitness_class})
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.save()
+
+            login(request, user)
+
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+    
+    return render (request, "registration/register.html", {"form": form})
