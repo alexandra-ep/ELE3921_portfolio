6861e3c202963fdad6c9eef3a4688ea3f34bbc92
Alexandra Eline Pettersen <57686601+alexandra-ep@users.noreply.github.com>
2026-04-01 15:53:32 +0200

    Update Instructor information: Add image and e-mail address, and run migrations

--- elevate/models.py
+++ elevate/models.py
@@ -6,6 +6,8 @@ class Instructor(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     bio = models.TextField(blank=True)
+    email = models.EmailField(blank=True)
+    image = models.ImageField(upload_to="instructors/", blank=True, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
